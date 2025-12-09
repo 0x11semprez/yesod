c431a9f0feca665525f450caacaeee6ba24a6870
0x11semprez <traoresemprez@icloud.com>
2025-12-09 08:06:06 +0100

    feat(ownable) : start Ownable with Yul for gas optimization:

---
+++ contracts/src/ownlib/Ownable.sol
@@ -0,0 +1,59 @@
+//SPDX-Licence-Identifier: MIT
+pragma solidity ^0.8.27;
+
+//@title Ownable Contract By Me
+//@dev This not recommended to create your own lib Ownable or SafeERC20 or whatever it is because you could make mistakes, so better to use something who works without a doubt, Trust OpenZellin
+
+contract Ownable {
+
+    //@notice Define error Handling in contracts
+    //@dev Better for gas optimisation and code understaning
+    error InvalidOwner(); //keccak256 0x49e27cffb37b1ca4a9bf5318243b2014d13f940af232b8552c208bdea15739da
+    error NotOwner(); //keccak256 0x30cd74712f59d478562d48e2d35de830db72c60a63dd08ae59199eec990b5bc4
+
+    //@dev Event declaration
+    event TransferOwnership(address indexed oldOwner, address indexed newOwner);
+
+    address private _owner;
+
+    constructor(address initialOwner) {
+        if (initialOwner == address(0)){revert InvalidOwner(address(0));}
+        transferOwnership (initialOwner);
+    }
+
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    function owner() public view returns(address result) {
+        assembly {
+            result := sload(_owner.slot)
+        }
+    }
+    
+
+    function _checkOwner() internal returns(bool) {
+        assembly {
+            if iszero(eq(caller(), sload(_owner.slot))) {
+                mstore(0x00, 0x990b5bc4)
+                revert(0x1c, 0x04)
+            }
+        }
+
+    }
+
+    function transferOwnership(address newOwner) internal onlyOwner{
+        assembly {
+            if iszero(shl(96, newOwner)) {
+                mstore(0x00, 0xa15739da)
+                revert(0x1c, 0x04)
+            }
+        }
+        address oldOwner = _owner;
+        _owner =  newOwner;
+        emit TransferOwnership(oldOwner, _owner);
+    }
+
+    
+}
