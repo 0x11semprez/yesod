9ae6ba095b338a07823919440ba62a3aae4fe3c5
0x11semprez <traoresemprez@icloud.com>
2025-12-10 07:38:57 +0100

    feat(add frontend): start frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <> 
+    <h1 className="text-gray-700 dark:text-gray-400">Hello</h1>
+    </>
+  );
+};
+
+export default App
