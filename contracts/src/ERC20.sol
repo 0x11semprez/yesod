19ec6d9ab6e411dccb8b354a170e04c2c50e829a
0x11semprez <traoresemprez@icloud.com>
2025-12-20 13:33:50 +0100

    feat: analyse things

--- contracts/src/ERC20.sol
+++ contracts/src/ERC20.sol
@@ -1,33 +1,41 @@
 //SPDX-Licence-Identifier: MIT
-pragma solidity 0.8.27;
+pragma solidity ^0.8.27;
 
-contract ERC20 {
+import "./Ownable.sol";
+
+contract ERC20 is Ownable {
     //@notice You can see all errors here;
-    //@dev error handling help you to spend less gas, we need keccak256 because we'll use Yul later.;
+    //@dev error handling help you to spend less gas;
 
-    error notEnoughETHInYourWallet(); //keccak256 : 0x49661fc744d3a721f3d40a93283704159489576e5de9f2454fe8c1e87afc78eb
-    error transactionFailed(); //keccak256 : 0xdccf81a9a2a82c9da94f3d3b593fc9afc9d89f9702a4a18c04b0ad53799c7120
+    error notEnoughETHInYourWallet(); 
+    error transactionFailed(); 
     error notEnoughAllowToSpend();
-    error  insufficientFunds();//keccak256 0x952d78c4b28c81a938fc7be8bf876b23e880e7620854eee740c78ce3fb37dadb
+    error  insufficientFunds();
+    error overflowError();
 
 
-    //@notice It is for all dev front and back can receive informartion;
     //@dev As you might know, just events;
     event Approval(address indexed owner, address indexed spender, uint amount);
     event Transfer(address indexed from, address indexed to, uint amount);
 
-    uint private _totalSupply = 10000000;
+    //@dev I choose a pre definied supply;
+    uint private _totalSupply = 10000000; 
 
-    mapping(address => uint) private _balances;
-    mapping(address => mapping(address => uint)) private _allowances;
+    mapping(address account => uint amount) private _balances;
+    mapping(address account => mapping(address account => uint amount)) private _allowances;
 
     constructor(address owner) {
         owner = msg.sender;
         _balances[owner] += _totalSupply;
     }
+
     //@notice Some views functions.
     //@dev better to use that because of the control.
-    function getTOKENNAME() public view returns(string memory) {
+    function getTOKENNAME() 
+        public 
+        view 
+        returns(string memory) 
+    {
         //@dev remember, strings are special so you need to use nmemory amd as we hardwrire with constamt.
         assembly {
             let TOKENNAME := mload(0x40) 
@@ -40,7 +48,11 @@ contract ERC20 {
     }
 
 
-    function getTOKENSYMBOL() public view returns(string memory) {
+    function getTOKENSYMBOL() 
+        public 
+        view 
+        returns(string memory) 
+    {
         assembly {
             let TOKENSYMBOL := mload(0x40)
             mstore(TOKENSYMBOL, 0x20)
@@ -51,21 +63,35 @@ contract ERC20 {
         }
     }
 
-    function getDECIMAL() public view returns(uint) {
+    function getDECIMAL() 
+        public 
+        view 
+        returns(uint) 
+    {
         return 18;
     }
 
-    function getTOTALSUPPLY() public view returns(uint) {
+    function getTOTALSUPPLY() 
+        public 
+        view 
+        returns(uint) 
+    {
         return 1000000;
     }
 
-    function getBALANCE(address account) public view returns(uint balanceaccount) {
+
+    function getBALANCE(address account) 
+        public 
+        view 
+        returns(uint balanceaccount) 
+    {
         assembly {
-            mstore(0x00, owner)
+            mstore(0x00, caller())
             mstore(0x20, _balances.slot)
             balanceaccount := sload(keccak256(0x00, 0x40))
         }
     }
+
 
     function _transfer(address from, address to, uint amount) 
         private 
@@ -75,14 +101,23 @@ contract ERC20 {
             mstore(0x00, from)
             mstore(0x20, _balances.slot)
             let balanceFrom := sload(keccak256(0x00, 0x40))
+
+            mstore(0x00, to)
+            mstore(0x20, _balances.slot)
+            let balanceTo := sload(keccak256(0x00, 0x40))
+
             if lt(balanceFrom, amount) {
                 mstore(0x00, 0x7afc78eb)
                 revert(0x1c, 0x04)
             }
+
+            let balanceFromAfter := sub(balanceFrom, amount)
+            let balanceToAfter := add(balanceTo, amount)
+
+            sstore(balanceFrom, balanceFromAfter)
+            sstore(balanceTo, balanceToAfter)           
         }
 
-        _balances[from] -= amount;
-        _balances[to] += amount;
         emit Transfer(from, to, amount);
         return true;
 
@@ -92,30 +127,50 @@ contract ERC20 {
         public
         returns(bool) 
     {
-        address from = msg.sender();
+        address from = msg.sender;
         _transfer(from, to, amount);
         emit Transfer(from, to, amount);
         return true;
     }
 
-    function transferFrom(address owner, address spender, uint256 amount) public returns (bool) {
-        address spender = msg.sender();
+    function transferFrom(address owner, address spender, uint256 amount) 
+        public 
+        returns(bool) 
+    {
+        address spender = msg.sender;
         uint newAllowance;
         assembly {
             mstore(0x00, owner)
-            mstore(0x20, spender)
-            mstore(0x40, _allowances.slot)
-            let _allowance := sload(keccak256(0x00, 0x60))
+            mstore(0x20, _allowances.slot)
+            let innerSlot := keccak256(0x00,0x40)
+
+            mstore(0x40, innerSlot)
+            mstore(0x60, spender)
+            let _allowance := sload(keccak256(0x40, 0x80))
+
+            mstore(0x00, owner)
+            mstore(0x20, _balances.slot)
+            let balanceOwner := sload(keccak256(0x00, 0x40))
+
+            mstore(0x00, spender)
+            mstore(0x20, _balances.slot)
+            let balanceSpender := sload(keccak256(0x00, 0x40))
+
             if lt(_allowance, amount) {
                 mstore(0x00, 0xfb37dadb)
                 revert(0x1c, 0x04)
             }
-            newAllowance := sub(_allowance, amount)
-        }
 
-        _allowances[owner][spender] = newAllowance;
-        _balances[owner] -= amount;
-        _balances[spender] += amount;
+            let newAllowance := sub(_allowance, amount)
+            sstore(_allowance, newAllowance)
+
+            let balanceOwnerAfter := sub(balanceOwner, amount)
+            sstore(balanceOwner, balanceOwnerAfter)
+
+            let balanceSpenderAfter := add(balanceSpender, amount)
+            sstore(balanceSpender, balanceSpednerAfter)
+
+        }
         
         _transfer(owner, spender, amount);
         return true;
@@ -124,13 +179,19 @@ contract ERC20 {
     function allowance(address owner, address spender) 
         public 
         view 
-        returns(uint256 allo) 
+        returns(uint) 
     {
         assembly {
             mstore(0x00, owner)
-            mstore(0x20, spender)
-            mstore(0x40, _allowances.slot)
-            allo := sload(keccak256(0x00, 0x60))
+            mstore(0x20, _allowances.slot)
+            let innerSlot := keccak256(0x00, 0x40)
+
+            mstore(0x00, spender)
+            mstore(0x20, innerSlot)
+            let allo := sload(keccak256(0x00, 0x40))
+
+            mstore(0x00, allo)
+            return(0x00, 32)
         }
 
     }
@@ -139,13 +200,59 @@ contract ERC20 {
         external 
         returns(bool) 
     {
-        address owner = msg.sender();
+        address owner = msg.sender;
         _allowances[owner][spender] = amount;
-        emit Approval(owmer, spender, amount);
+        emit Approval(owner, spender, amount);
+
+        return true;
+    }
+    
+    function _mint(uint amount) 
+        external 
+        onlyOwner
+        returns(bool)
+    {
+        assembly {
+            let _TotalSupply := sload(_totalSupply.slot)
+
+            mstore(0x00, _TotalSupply)
+            mstore(0x20, _balances.slot)
+
+            let balanceBefore := sload(keccak256(0x00, 0x40))
+            let balanceAfter := add(balanceBefore, amount)
+
+            if lt(balanceAfter, balanceBefore) {
+                mstore(0x00, 0x792bbe49)
+                revert(0x1c, 0x04)
+            }
+        }
+
         return true;
     }
 
-    function _mint() internal {}
-    function _burn() internal {}
+    function _burn(uint amount) 
+        external 
+        onlyOwner 
+        returns(bool)
+    {
+        assembly {
+            let _TotalSupply := sload(_totalSupply.slot)
 
+            mstore(0x00, _TotalSupply)
+            mstore(0x20, _balances.slot)
+
+            let balanceBefore := sload(keccak256(0x00, 0x40))
+            let balanceAfter := sub(balanceBefore, amount)
+
+            if gt(balanceAfter, balanceBefore) {
+                mstore(0x00, 0x792bbe49)
+                revert(0x1c, 0x04)
+            }
+        }
+
+        return true;
+    }
+
+    receive() external payable {}
+    fallback() external payable {}
 }
