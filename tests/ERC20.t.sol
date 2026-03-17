a60f6ce6645a2e1f918afc6ed1ba8e20fdb17903
0x11semprez <traoresemprez@icloud.com>
2026-03-17 09:49:26 +0100

    feat:

---
+++ tests/ERC20.t.sol
@@ -0,0 +1,7 @@
+//// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+import {Test} from "forge-std/Test.sol";
+import {ERC20} from "../contracts/ERC20.sol";
+
+contract ERC20Test is ERC20 {}
